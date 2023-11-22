5a11ae31d14dc0e2c9bf5436b8a3ee8f65fa5001
Uliana <uliana.shpineva@gmail.com>
2023-11-22 21:28:20 +0300

    ..

--- solution/include/utils.h
+++ solution/include/utils.h
@@ -1,7 +1,10 @@
-#include "image.h"
+#ifndef UTILS
+#define UTILS
+
 #include "bmp.h"
 #include <stdbool.h>
 
 bool create_image(struct image* img, uint64_t width, uint64_t height);
 void print_read_bmp_status(enum read_status read_status);
 void print_write_bmp_status(enum write_status write_status);
+#endif
